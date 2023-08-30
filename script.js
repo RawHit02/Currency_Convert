3984a139f6ffd67b3b61b0e061bac3d36c22c149
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-08-30 13:20:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const dropList = document.querySelectorAll("form select"),
+  fromCurrency = document.querySelector(".from select"),
+  toCurrency = document.querySelector(".to select"),
+  getButton = document.querySelector("form button"),
+  exchangeRateTxt = document.querySelector(".exchange-rate");
+
+for (let i = 0; i < dropList.length; i++) {
+  for (let currency_code in country_list) {
+    let selected =
+      i === 0
+        ? currency_code === "USD"
+          ? "selected"
+          : ""
+        : currency_code === "NPR"
+        ? "selected"
+        : "";
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target);
+  });
+}
+
+function loadFlag(element) {
+  for (let code in country_list) {
+    if (code === element.value) {
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+    }
+  }
+}
+
+window.addEventListener("load", () => {
+  getExchangeRate();
+});
+
+getButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", () => {
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
+});
+
+function getExchangeRate() {
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+
+  // Construct the URL with the proper base currency
+  const url = `https://v6.exchangerate-api.com/v6/33d923d521378eee1a446e3e/latest/${fromCurrency.value}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.result === "success") {
+        const exchangeRate = result.conversion_rates[toCurrency.value];
+        const amountVal = parseFloat(document.getElementById("amountInput").value);
+        const totalExRate = (amountVal * exchangeRate).toFixed(2);
+
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+      } else {
+        exchangeRateTxt.innerText = "Currency conversion not available";
+      }
+    })
+    .catch(() => {
+      exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
